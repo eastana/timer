af459c8ddf56444dbcbab925ea342c59094bb5f6
kuanyshbay_i <ibragim.kuanyshbay@kaspi.kz>
2023-03-15 14:44:28 +0600

    Fixed logic of naming day/days

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + " days " + hours + "h "
+
+  let namingDays = days > 1 ? "days" : "day";
+
+  document.getElementById("demo").innerHTML = days + namingDays + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
